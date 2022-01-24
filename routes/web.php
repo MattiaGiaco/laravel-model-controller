1aef83e173386765ff1544992222a5c6a6f2594b
mattiagiaco <martex781227@gmail.com>
2022-01-24 16:46:58 +0100

    controller

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/', 'PageController@index')->name('home');
 
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
+Route::get('/about', 'PageController@about')->name('about');
 
-Route::get('/contacts', function () {
-    return view('contacts');
-})->name('contacts');
+Route::get('/contacts','PageController@contacts')->name('contacts');
